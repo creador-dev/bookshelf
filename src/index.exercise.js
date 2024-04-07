0f823b708bf605e84356395279a0f4f5ce9e5b24
Pawan Kumar <pwnkumar201@gmail.com>
2024-04-07 13:05:39 +0530

    ✨ new(ex-01): rendering modal on click of register and login button.

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,18 +1,34 @@
+import '@reach/dialog/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Dialog} from '@reach/dialog'
 import {Logo} from './components/logo'
 
 const App = () => {
+  const [openModal, setOpenModal] = React.useState('none')
+
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('login clicked.')}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={() => alert('register clicked.')}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label="Login form" isOpen={'login' === openModal}>
+        <div>
+          <h3>Login Modal</h3>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+      </Dialog>
+      <Dialog aria-label="Registration form" isOpen={'register' === openModal}>
+        <div>
+          <h3>Registration Modal</h3>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+      </Dialog>
     </div>
   )
 }
